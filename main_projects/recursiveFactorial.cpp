3d26ba2b04dcd14cbb8bf6f72fdd35a316a4f7f7
TenaCity23 <121407811+TenaCity23@users.noreply.github.com>
2023-01-05 16:23:59 -0800

    Create recursiveFactorial.cpp

---
+++ main_projects/recursiveFactorial.cpp
@@ -0,0 +1,27 @@
+//============================================================================
+// Name        : RecursiveFactorial.cpp
+// Author      : TenaCity23
+// Description : Recursive function that outputs the factorial of a given number
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int factorial (int n) {
+	if (n <= 1) {
+		return 1;
+	} else {
+		int result = n * factorial(n-1);
+		return result;
+	}
+}
+
+int main() {
+	int result;
+	int n = 4;
+  
+	result = factorial(n);
+	cout << "Factorial of " << n << " is " << result << endl;
+  
+	return 0;
+}
