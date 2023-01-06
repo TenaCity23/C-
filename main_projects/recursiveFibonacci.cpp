2a69e7fc01d6760efd15ca6476153f2a775f7701
TenaCity23 <121407811+TenaCity23@users.noreply.github.com>
2023-01-05 16:39:13 -0800

    Create recursiveFibonacci.cpp

---
+++ main_projects/recursiveFibonacci.cpp
@@ -0,0 +1,43 @@
+//============================================================================
+// Name        : recursiveFibonacci.cpp
+// Author      : TenaCity23
+// Description :
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int fibonaci (int n) {
+	int result;
+
+	for (int i = 1; i < n; i++) {
+		result = i + i;
+	}
+
+	return result;
+}
+
+
+int main() {
+
+	int fib;
+	int m = 2;
+    
+	fib = fibonaci(m);
+	cout << "Fibonnaci Sequence of 1: " << fib << endl;
+
+	int fib2;
+	int j = 5;
+    
+	fib2 = fibonaci(j);
+	cout << "Fibonnaci Sequence of 5: " << fib2 << endl;
+
+	return 0;
+}
+
+/* Output: 
+
+Fibonnaci Sequence of 1: 2
+Fibonnaci Sequence of 5: 8
+
+*/
