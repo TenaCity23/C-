a5747487b27b6bbffce87631c65582ca625188ad
TenaCity23 <121407811+TenaCity23@users.noreply.github.com>
2023-01-09 01:33:28 -0800

    Create palindrome.cpp

---
+++ other_projects/palindrome.cpp
@@ -0,0 +1,41 @@
+//============================================================================
+// Name        : palindrome.cpp
+// Author      : TenaCity23
+// Description : Checking for palindrome!!
+//============================================================================
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool is_palindrome(std::string text) {
+  
+    string reversed_text = "";
+    
+    for (int i = text.size() - 1; i >= 0; i--) {
+        reversed_text += text[i];
+    }
+    
+    if (reversed_text == text) {
+        return true;
+    }
+    
+    return false;
+}
+
+int main() {
+  
+    cout << is_palindrome("madam") << "\n";
+    cout << is_palindrome("ada") << "\n";
+    cout << is_palindrome("lovelace") << "\n";
+
+    return 0;
+}
+
+/* Final Output: 
+
+1
+1
+0
+
+*/
