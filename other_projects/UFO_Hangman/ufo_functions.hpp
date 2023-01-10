b6dda07c731fa10fe1805c32dbc7557b3fb9db1e
TenaCity23 <121407811+TenaCity23@users.noreply.github.com>
2023-01-10 03:12:24 -0800

    Create ufo_functions.hpp

---
+++ other_projects/UFO_Hangman/ufo_functions.hpp
@@ -0,0 +1,11 @@
+#include <vector>
+#include <string>
+using namespace std; 
+
+void greet ();
+
+void display_misses(int misses);
+
+void display_status(vector<char>, string);
+
+void end_game(string, string);
