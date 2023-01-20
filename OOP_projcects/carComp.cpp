7a6b477671f00a2466093830dc4f9d7f8c2f38b4
An Nguyen <121407811+TenaCity23@users.noreply.github.com>
2023-01-20 11:16:53 -0800

    Create carComp.cpp

---
+++ OOP_projcects/carComp.cpp
@@ -0,0 +1,50 @@
+//============================================================================
+// Name        : carComp.cpp
+// Author      : TenaCity23
+// Description : Comparing Cars using Classes
+//============================================================================
+
+#include <iostream> 
+#include "car.hpp"
+using namespace std; 
+
+int main () { 
+
+    int x = 5; 
+    int y = 7; 
+
+    Car myCar {"Toyota", 2000, 5000};
+    Car yourCar {"Honda", 2018, 10000};
+
+    cout << "My Car specs: " << endl << myCar << endl;
+    cout << "Your Car specs: " << endl << yourCar << endl;
+
+    if (myCar > yourCar) { 
+        cout << "My car is more expensive than your car." << endl;
+    } else { 
+        cout << "Your car is more expensive than my car." << endl;
+    }
+
+    double total = myCar + yourCar; //myCar.operator+(yourCar)
+
+    cout << "Total Value of both our cars: $" << total << endl; 
+
+    return 0;
+}
+
+/* Output: 
+
+My Car specs: 
+Make: Toyota
+Year: 2000
+Price: $5000
+
+Your Car specs: 
+Make: Honda
+Year: 2018
+Price: $10000
+
+Your car is more expensive than my car.
+Total Value of both our cars: $15000
+
+*/
